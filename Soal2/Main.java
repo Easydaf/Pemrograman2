3febe1fec0536752d5b44b4d2107dc5d3835c339
Easydaf <144783125+Easydaf@users.noreply.github.com>
2024-11-25 22:04:56 +0800

    Add files via upload

---
+++ Soal2/Main.java
@@ -0,0 +1,38 @@
+package Soal2;
+import java.util.LinkedList;
+import java.util.Scanner;
+public class Main {
+public static void main(String[] args) {
+    LinkedList<Negara> negaraList = new LinkedList<Negara>();
+    Scanner input = new Scanner(System.in);
+    System.out.print("Masukkan jumlah negara: ");
+    int jumlahNegara = input.nextInt();
+    input.nextLine();
+    for (int i=0; i<jumlahNegara; i++) {
+        System.out.print("Nama Negara: ");
+        input.next();
+        String nama = input.nextLine();
+        System.out.print("Jenis Kepemimpinan: ");
+        String jenisKePemimpinan = input.nextLine();
+        System.out.print("Nama Pemimpin: ");
+        String namaPemimpin = input.nextLine();
+        if (jenisKePemimpinan.equals("monarki")) {
+            negaraList.add(new Negara(nama, jenisKePemimpinan, namaPemimpin));
+        }
+        else {
+            System.out.print("Tanggal Merdeka: ");
+            int tanggalMerdeka = input.nextInt();
+            System.out.print("Bulan Merdeka: ");
+            int bulanMerdeka = input.nextInt();
+            System.out.print("Tahun Merdeka: ");
+            int tahunMerdeka = input.nextInt();
+            negaraList.add(new Negara(nama, jenisKePemimpinan, namaPemimpin, tanggalMerdeka, bulanMerdeka, tahunMerdeka));
+        }
+    }
+    for (int i=0; i<negaraList.size(); i++) {
+        Negara negara = negaraList.get(i);
+        negara.display();
+    }
+
+}
+}
