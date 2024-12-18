1da7bfe7ddc9371ea74a47fba4ad09cbcba3a605
Easydaf <144783125+Easydaf@users.noreply.github.com>
2024-12-18 21:44:37 +0800

    Add files via upload

---
+++ Soal1Prak5/Shape.java
@@ -0,0 +1,15 @@
+package Soal1Prak5;
+
+public abstract class Shape {
+    private String shapeName;
+    public Shape(String shapeName) {
+        this.shapeName = shapeName;
+    }
+
+    public abstract double area();
+
+    @Override
+    public String toString() {
+        return shapeName;
+    }
+}
